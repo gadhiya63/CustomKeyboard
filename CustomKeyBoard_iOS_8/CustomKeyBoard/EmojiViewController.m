cec10bc03972ba18502c0bec939d8cb35a7ab819
gadhiya63 <pankaj.gadhiya@nbcuni.com>
2016-10-16 14:35:43 -0400

    add file in new emoji branch

---
+++ CustomKeyBoard_iOS_8/CustomKeyBoard/EmojiViewController.m
@@ -0,0 +1,39 @@
+//
+//  EmojiViewController.m
+//  CustomKeyBoard
+//
+//  Created by Pankaj206448782 on 10/16/16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import "EmojiViewController.h"
+
+@interface EmojiViewController ()
+
+@end
+
+@implementation EmojiViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    //emoji view controller
+    //add new files in emoji branch
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
